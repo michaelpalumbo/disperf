ddb8a045249cbb4c514453463b7a35ac1b7a6134
thedispersionlab <emailmichaelpalumbo@gmail.com>
2018-10-01 11:12:45 -0400

    added a browser client for viewing realtime report data in a d3 line chart

--- disperf.js
+++ disperf.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
-  
+const express = require('express'); 
 const fs = require("fs");
 const { exec } = require('child_process')
 const sendmail = require('sendmail')()
 const date = require('date-and-time');
 const shell = require("vorpal")();
+const WebSocket = require('ws');
+const http = require('http');
+const url = require('url');
+
+const path = require("path");
+
 
 var reportJSON = [];
 // let now = new Date();
@@ -254,3 +260,105 @@ scheduler();
 //     })
 //   console.log(reportJSON)
 // }
+
+
+const client_path = path.join(__dirname + '/client')
+// client hosting
+const app = express();
+app.use(express.static(client_path))
+app.get('/', function(req, res) {
+	res.sendFile(path.join(client_path, 'index.html'));
+});
+//app.get('*', function(req, res) { console.log(req); });
+const server = http.createServer(app);
+// add a websocket service to the http server:
+const wss = new WebSocket.Server({ server });
+
+// launch the client in chrome:
+exec('/usr/bin/open -a "/Applications/Google Chrome.app" \'http://localhost:8080\'')
+
+// send a (string) message to all connected clients:
+function send_all_clients(msg) {
+	wss.clients.forEach(function each(client) {
+		client.send(msg);
+	});
+}
+
+
+// whenever a client connects to this websocket:
+wss.on('connection', function(ws, req) {
+  // it defines a new session:
+let session = {
+  id: sessionId++,
+  socket: ws,
+};
+sessions[session.id] = session;
+console.log("server received a connection, new session " + session.id);
+console.log("server has "+wss.clients.size+" connected clients");
+
+const location = url.parse(req.url, true);
+// You might use location.query.access_token to authenticate or share sessions
+// or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+
+ws.on('error', function (e) {
+  if (e.message === "read ECONNRESET") {
+    // ignore this, client will still emit close event
+  } else {
+    console.error("websocket error: ", e.message);
+  }
+});
+
+// what to do if client disconnects?
+ws.on('close', function(connection) {
+  console.log("session", session.id, "connection closed");
+  delete sessions[session.id];
+});
+
+// respond to any messages from the client:
+ws.on('message', function(e) {
+  //console.log(e)
+  if(e instanceof Buffer) {
+    // get an arraybuffer from the message:
+    const ab = e.buffer.slice(e.byteOffset,e.byteOffset+e.byteLength);
+    console.log("session", session.id, "received arraybuffer", ab);
+    // as float32s:
+    console.log(new Float32Array(ab));
+  } else {
+    // get JSON from the message:
+    try {
+      let msg = JSON.parse(e);
+      console.log("session", session.id, "received JSON", msg);
+      handleMessage(msg, session);
+
+    } catch (e) {
+      console.log('bad JSON: ', e);
+    }
+  }
+});
+
+// // Example sending binary:
+// const array = new Float32Array(5);
+// for (var i = 0; i < array.length; ++i) {
+// 	array[i] = i / 2;
+// }
+// ws.send(array);
+});
+
+server.listen(8080, function() {
+console.log('server listening on %d', server.address().port);
+});
+
+function handleMessage(msg, session) {
+	console.log("message from client: " + msg)
+	switch (msg.type) {
+
+		case "something": {
+			// do this
+		}
+		break;
+
+		break
+		
+	}
+}
